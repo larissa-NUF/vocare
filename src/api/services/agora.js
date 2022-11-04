95d1082d460a9f54acaf07888fe264856ebfa0fc
larissa-NUF <larissanuf.figueredo@gmail.com>
2022-11-04 20:41:09 -0300

    foi?

--- src/api/services/agora.js
+++ src/api/services/agora.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks, SDK_CODEC, SDK_MODE } fr
 
 const appId = "3fe2a01586fa4e7e949c6d9ac3b847f2";
 const token =
-  "007eJxTYDh79i/Tsj1GgcKLPP6sKtA5Gv5FnKli8k12r0lVi3Z0fIlXYDBOSzVKNDA0tTBLSzRJNU+1NLFMNkuxTEw2TrIwMU8z0mNNTW4IZGQosuVjZWSAQBCfhSE3MTOPgQEAaioerA==";
+  "007eJxTYHBa/HqxylTDK+EBN5WP2p2Sld13i2MnV91Csaa4LdN1VUUVGIzTUo0SDQxNLczSEk1SzVMtTSyTzVIsE5ONkyxMzNOMkhelJjcEMjK8Of+ZiZEBAkF8FobcxMw8BgYA/rEfFQ==";
 
 export const config = { mode: "rtc", codec:"vp8", appId: appId, token: token };
 export const useClient = createClient(config);
